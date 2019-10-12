5eecee787c43c637b2ef275a695c9a82884207ec
matfax <matthias.fax@gmail.com>
2019-10-12 20:51:03 +0200

    Run Windows path tests only on Windows

--- tests/test_immutapath.py
+++ tests/test_immutapath.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from mutapath import Path
@@ -10,9 +11,10 @@ class TestPath(unittest.TestCase):
         self.assertEqual(expected.normpath(), actual.normpath())
 
     def test_with_name_win(self):
-        expected = Path("C:/B/other")
-        actual = Path("C:/B/test1.txt").with_name("other")
-        self.assertEqual(expected.abspath(), actual.abspath())
+        if os.name == 'nt':
+            expected = Path("C:/B/other")
+            actual = Path("C:/B/test1.txt").with_name("other")
+            self.assertEqual(expected.abspath(), actual.abspath())
 
     def test_with_base_posix(self):
         expected = Path("/home/joe/folder/sub")
@@ -25,11 +27,13 @@ class TestPath(unittest.TestCase):
         self.assertEqual(expected.normpath(), actual.normpath())
 
     def test_with_base_win(self):
-        expected = Path("C:/Users/joe/folder/sub")
-        actual = Path("C:/Users/doe/folder/sub").with_base("C:/Users/joe")
-        self.assertEqual(expected.normpath(), actual.normpath())
+        if os.name == 'nt':
+            expected = Path("C:/Users/joe/folder/sub")
+            actual = Path("C:/Users/doe/folder/sub").with_base("C:/Users/joe")
+            self.assertEqual(expected.normpath(), actual.normpath())
 
     def test_with_base_length_win(self):
-        expected = Path("C:/Users/joe/doe/folder/sub").abspath()
-        actual = Path("C:/Users/doe/folder/sub").abspath().with_base("C:/Users/joe", 1)
-        self.assertEqual(expected.normpath(), actual.normpath())
+        if os.name == 'nt':
+            expected = Path("C:/Users/joe/doe/folder/sub").abspath()
+            actual = Path("C:/Users/doe/folder/sub").abspath().with_base("C:/Users/joe", 1)
+            self.assertEqual(expected.normpath(), actual.normpath())
