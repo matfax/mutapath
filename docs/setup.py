4441c86475aa219dc63e1670c44b4d2f8e210299
matfax <matthias.fax@gmail.com>
2019-10-19 18:57:00 +0200

    docs: closes #7

--- docs/setup.py
+++ docs/setup.py
@@ -1,6 +1,25 @@
+import json
+from typing import List, Optional
+
 from setuptools import setup, find_packages
 
-from setup import get_dependencies
+from mutapath import Path
+
+
+def get_dependencies(pipfile_lock: Optional[str] = None, develop: bool = False):
+    if pipfile_lock is None:
+        pipfile_lock = Path("Pipfile.lock")
+    lock_data = json.load(pipfile_lock.open())
+    result: List[str] = [package_name for package_name in lock_data.get('default', {}).keys()]
+    if develop:
+        result += [package_name for package_name in lock_data.get('default', {}).keys()]
+    for k in result:
+        if "path-py" in k:
+            new_key = k.replace("path-py", "path.py")
+            result.remove(k)
+            result.append(new_key)
+    return result
+
 
 setup(
     name="mutapath",
--- setup.py
+++ setup.py
@@ -1,24 +1,8 @@
-import json
 from pathlib import Path
-from typing import List
 
 from setuptools import setup, find_packages
 
-
-def get_dependencies(pipfile_lock=None, develop: bool = False):
-    if pipfile_lock is None:
-        pipfile_lock = Path("Pipfile.lock")
-    lock_data = json.load(pipfile_lock.open())
-    result: List[str] = [package_name for package_name in lock_data.get('default', {}).keys()]
-    if develop:
-        result += [package_name for package_name in lock_data.get('default', {}).keys()]
-    for k in result:
-        if "path-py" in k:
-            new_key = k.replace("path-py", "path.py")
-            result.remove(k)
-            result.append(new_key)
-    return result
-
+from docs.setup import get_dependencies
 
 setup(
     name="mutapath",
