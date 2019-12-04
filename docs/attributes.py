54df642c7472bf030601e294cb9dada334afeec5
matfax <matthias.fax@gmail.com>
2019-12-04 15:23:53 +0100

    style: enforce black for docs package

--- docs/attributes.py
+++ docs/attributes.py
@@ -12,13 +12,15 @@ LOG = logging.getLogger(__name__)
 _DOC_REF_MATCHER = re.compile(r'[ "]*[.]{2} seealso:: :[funcatr]+:`([a-zA-Z.]+)`[ "]*$')
 
 
-def _import_wrapped_members(cls, member_name, member, cls_name=''):
+def _import_wrapped_members(cls, member_name, member, cls_name=""):
     LOG.debug(f"[attributes] inspecting {cls_name}.{member_name}")
     if not member_name.startswith("_"):
         see_other_matched = _DOC_REF_MATCHER.match(str(member.__doc__))
         if see_other_matched is not None:
             other_ref = see_other_matched.group(1)
-            LOG.info(f"[attributes] importing referenced doc for member {cls_name}.{member_name} from {other_ref}")
+            LOG.info(
+                f"[attributes] importing referenced doc for member {cls_name}.{member_name} from {other_ref}"
+            )
             try:
                 other_obj = import_object(other_ref)
                 member.__doc__ = other_obj.__doc__
@@ -36,18 +38,18 @@ def setup(app: Sphinx):
     app.add_autodocumenter(AppDocumenter, override=True)
 
     for name, member in inspect.safe_getmembers(mutapath.Path):
-        _import_wrapped_members(mutapath.Path, name, member, 'mutapath.Path')
+        _import_wrapped_members(mutapath.Path, name, member, "mutapath.Path")
     for name, member in inspect.safe_getmembers(mutapath.MutaPath):
-        _import_wrapped_members(mutapath.MutaPath, name, member, 'mutapath.MutaPath')
+        _import_wrapped_members(mutapath.MutaPath, name, member, "mutapath.MutaPath")
 
 
 class AppDocumenter(autodoc.ModuleDocumenter):
-
     def __init__(self, *args) -> None:
         super().__init__(*args)
         if isinstance(self.object, mutapath.Path):
             for member in dir(mutapath.Path):
-                if member not in self.options.get('members') \
-                        and not member.startswith("_"):
+                if member not in self.options.get("members") and not member.startswith(
+                        "_"
+                ):
                     LOG.info(f"[attributes] adding wrapped member {member}")
-                    self.options['members'].append(member)
+                    self.options["members"].append(member)
